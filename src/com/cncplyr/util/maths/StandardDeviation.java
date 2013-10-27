f904809309cf31857669f9de32ee042be1454249
Ric Jenkin <cncplyr@gmail.com>
2013-10-27 22:48:35 +0000

    Initial commit, taken from svn. Updated namespaces.

---
+++ src/com/cncplyr/util/maths/StandardDeviation.java
@@ -0,0 +1,46 @@
+package com.cncplyr.util.maths;
+
+import java.util.List;
+
+/**
+ * 
+ * @author Richard Jenkin
+ * @version 1.0
+ *
+ */
+public class StandardDeviation {
+
+	public StandardDeviation() {
+
+	}
+
+	public Integer findSDInt(List<Integer> input) {
+		AverageFinder af = new AverageFinder();
+		Integer mean = af.findMean(input);
+
+		Integer top = 0;
+		Integer n = input.size();
+
+		for (Integer i : input) {
+			i += (i - mean) ^ 2;
+		}
+		Integer SD = (int) Math.sqrt(top / n);
+
+		return SD;
+	}
+
+	public double findSDDouble(List<Double> input) {
+		AverageFinder af = new AverageFinder();
+		double mean = af.findMeanDouble(input);
+
+		double top = 0.0f;
+		double n = input.size();
+
+		for (double i : input) {
+			top += Math.pow(i - mean, 2.0d);
+		}
+		double SD = Math.sqrt(top / n);
+
+		return SD;
+	}
+}
